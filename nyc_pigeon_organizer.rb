40ac29d6b4d99dee4eb86ebf1507f68638c6c8e8
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-05 18:42:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,49 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  names = []
+  
+  data.each{ |key, value|
+    value.each{ |key2, value2|
+      names += value2 
+    }
+  }
+  
+  uniq_names = names.uniq
+  
+  data.each{ |key, value|
+    value.each{ |key2, value2|
+      uniq_names.each{ |name|
+        temp = []
+        {
+          name => {key => temp.push 
+          }
+        }
+        
+      }
+    }
+  
+
+  
 end
+
+nyc_pigeon_organizer(pigeon_data)
